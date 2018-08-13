a38df6e717f6935d47fb981d549000356896da25
Anton Lammert <anton.lammert@fernau-it-solutions.de>
2018-08-13 15:13:59 +0200

    Added a box deserialize.

--- framework/scene.hpp
+++ framework/scene.hpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include "material.hpp"
+#include "box.hpp"
 #include <vector>
 #include <set>
 #include <map>
@@ -10,6 +11,7 @@
 
 struct Scene {
 	std::map<std::string, std::shared_ptr<Material>> mat_map;
+	std::map<std::string, std::shared_ptr<Shape>> shape_map;
 };
 
 void deserializeObjects(Scene& scene, std::string line){
@@ -31,13 +33,33 @@ void deserializeObjects(Scene& scene, std::string line){
 			Color ks{std::stof(lineParts[9],NULL),std::stof(lineParts[10],NULL),std::stof(lineParts[11],NULL)};
 			float m{std::stof(lineParts[12],NULL)};
 			std::shared_ptr<Material>mat = std::make_shared<Material>(lineParts[2],ka,kd,ks,m); 
-			std::cout<<*mat<<"\n";
+			std::cout<<"Material: " << *mat<<"\n";
 			scene.mat_map.insert(std::pair<std::string,std::shared_ptr<Material>>(mat->name,mat));
 		}
 		catch(std::invalid_argument arg)
 		{	
 			std::cout<<"Something went wrong, while loading material. Check format!\n";
 			std::cout<<"Throws exception : "<<arg.what()<<"\n";
+		}
+	}
+	if (lineParts[1] == "shape")
+	{
+		std::cout << "Load shape ...\n";
+		if (lineParts[2] == "box") {
+			try {
+				std::string name = lineParts[3];
+				glm::vec3 min = glm::vec3{ std::stof(lineParts[4], NULL), std::stof(lineParts[5], NULL), std::stof(lineParts[6], NULL) };
+				glm::vec3 max = glm::vec3{ std::stof(lineParts[7], NULL), std::stof(lineParts[8], NULL), std::stof(lineParts[9], NULL) };
+				std::shared_ptr<Material> mat = scene.mat_map.at(lineParts[10]);
+				std::shared_ptr<Box> box = std::make_shared<Box>( min, max, name, mat);
+				scene.shape_map.insert(std::pair<std::string, std::shared_ptr<Shape>>(name, box));
+				std::cout << "Box: " << *box << "\n";
+			}
+			catch (std::invalid_argument arg)
+			{
+				std::cout << "Something went wrong, while loading the box. Check format!\n";
+				std::cout << "Throws exception : " << arg.what() << "\n";
+			}
 		}
 	}
 }
